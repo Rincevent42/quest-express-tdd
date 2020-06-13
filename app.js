eefea2303bbdfcb3433593a2168f4a3307ac7016
Rincevent42 <porvooc@gmail.com>
2020-06-13 13:47:23 +0200

    Etape 2/2 passée

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 // app.js
 const express = require('express');
 const bodyParser = require('body-parser');
+require('dotenv').config();
+const connection = require('./connection');
 
 const app = express();
 app.use(bodyParser.json());
@@ -8,6 +10,43 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get('/', (req, res) => {
   res.json({ message: 'Hello World!' });
+});
+
+app.post('/bookmarks', (req, res) => {
+  const { url, title } = req.body;
+  if (!title || !url) {
+    res.status(422).json({ error: 'required field(s) missing' });
+  } else {
+    return connection.query('INSERT INTO bookmark SET ?', req.body, (err, results) => {
+      if (err) {
+        // Si une erreur est survenue, alors on informe l'utilisateur de l'erreur
+        console.log(err);
+        res.status(500).send("Erreur lors de la sauvegarde d'un employé");
+      } else {
+        const insertId = results.insertId;
+        return connection.query('SELECT * FROM bookmark WHERE id = ?', insertId, (err2, records) => {
+          if (err2) {
+            return res.status(500).send('Error when retrived data from db');
+          }
+
+          const { title, url } = records[0];
+
+          return res
+            .status(201)
+            .json({
+              id: insertId, /* id: insertId, */ 
+              url: url,
+              title: title
+            });
+  
+        });
+
+      }
+
+
+    });
+  }
+
 });
 
 module.exports = app;
--- test/app.integration.spec.js
+++ test/app.integration.spec.js
@@ -14,4 +14,38 @@ describe('Test routes', () => {
         done();
       });
   });
+
+  it ('POST /bookmarks with missing title field', (done) => {
+    request(app)
+      .post('/bookmarks')
+      .send({ })
+      .expect(422)
+      .expect('Content-Type', /json/)
+      .then(response => {
+        const expected = { error: 'required field(s) missing' };
+        expect(response.body).toEqual(expected);
+        done();
+      });
+  });
+
+  it ('POST /bookmarks success', (done) => {
+    request(app)
+      .post('/bookmarks')
+      .send({ 
+        title: 'Jest',
+        url: 'https://jestjs.io'
+      })
+      .expect(201)
+      .expect('Content-Type', /json/)
+      .then(response => {
+        const expected = { 
+          id: 1,
+          url: 'https://jestjs.io',
+          title: 'Jest'
+        };
+        expect(response.body).toEqual(expected);
+        done();
+      });
+  });
+
 });
